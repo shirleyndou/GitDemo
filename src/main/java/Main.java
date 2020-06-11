82dab80ae8d6ca9808f76ca81d220fc444d42c26
shirleyndou <shirley.ndou@umuzi.org>
2020-06-11 21:57:03 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String tummy = "";
+
+        if(tummy.isEmpty()){
+            System.out.println("Eat an apple");
+        }
+    }
+}
